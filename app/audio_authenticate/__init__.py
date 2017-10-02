08cc90d328669a6f6c52bc33b02c4e633de79414
Nitesh Arora <nitesharora21@gmail.com>
2017-10-02 09:34:29 -0400

    Integrated auth and service with speech to text

--- app/audio_authenticate/__init__.py
+++ app/audio_authenticate/__init__.py
@@ -1,3 +1,5 @@
+import json
+
 import requests
 from PickupAI.config import SUBSCRIPTION_TOKEN
 
@@ -17,9 +19,8 @@ def create_profile():
 
     response = requests.request("POST", url, data=payload, headers=headers)
 
-    identificationProfileId = response.text['identificationProfileId']
-    enroll_user(identificationProfileId)
-    return response.text
+    identificationProfileId = json.loads(response.text).get("identificationProfileId")
+    return identificationProfileId
 
 
 def enroll_user(identificationProfileId, audio_file):
@@ -44,12 +45,63 @@ def enroll_user(identificationProfileId, audio_file):
 
 
 def get_all_profiles():
+    profile_id_list = []
     url = "https://westus.api.cognitive.microsoft.com/spid/v1.0/identificationProfiles"
     headers = {
         'ocp-apim-subscription-key': SUBSCRIPTION_TOKEN,
         'cache-control': "no-cache",
     }
     response = requests.request("GET", url, headers=headers)
+    for items in json.loads(response.text):
+        profile_id_list.append(items.get('identificationProfileId'))
+    return profile_id_list
 
     return response.text
 
+
+def identify_user(list_profileids, identify_audio_file):
+    url = "https://westus.api.cognitive.microsoft.com/spid/v1.0/identify"
+
+    querystring = {
+        "identificationProfileIds": list_profileids}
+
+    headers = {
+        'host': "westus.api.cognitive.microsoft.com",
+        'content-type': "application/octet-stream",
+        'ocp-apim-subscription-key': "dc1c63aec7fa4446961f56fba8e1aefa",
+        'cache-control': "no-cache",
+        'postman-token': "5513c734-afdc-cc69-aa79-9af10f831d9b"
+    }
+
+    response = requests.request("POST", url, headers=headers, params=querystring,
+                                data=open('../../' + identify_audio_file, 'rb'))
+    return response.headers.get('Operation-Location').split('/')[-1]
+
+
+def auth_user(oper_code, *args):
+    url = "https://westus.api.cognitive.microsoft.com/spid/v1.0/operations/933f699e-6f85-4c71-bdf9-e24d8b50799c"
+
+    # querystring = {"operationId": "a137ad81-1659-41c2-bca1-7d3351aad04a"}
+    querystring = {"operationId": oper_code}
+
+    headers = {
+        'host': "westus.api.cognitive.microsoft.com",
+        'ocp-apim-subscription-key': SUBSCRIPTION_TOKEN,
+        'cache-control': "no-cache",
+    }
+
+    response = requests.request("GET", url, headers=headers, params=querystring)
+
+    resp = json.loads(response.text).get('processingResult')
+    print response.text
+    try:
+        profile_id = resp.get('identifiedProfileId')
+        status = resp.get('confidence')
+        return {'Confidence': status, 'profile_id': profile_id}
+    except AttributeError:
+        return 'Not Found'
+
+
+if __name__ == '__main__':
+    # print identify_user(get_all_profiles())
+    print auth_user(identify_user(get_all_profiles()), 'auth')
--- app/test.py
+++ app/test.py
@@ -3,6 +3,7 @@ import os
 import re
 import sys
 import django
+from audio_authenticate import *
 
 sys.path.append(os.path.join(os.path.relpath(__file__), "..", ".."))
 os.environ['DJANGO_SETTINGS_MODULE'] = 'PickupAI.settings'
@@ -13,7 +14,6 @@ from audio_text_parser.audio_text_parser import AudioTextParser
 
 
 def process_text_service(parsed_text):
-
     status = parsed_text.get("RecognitionStatus")
 
     if status == "Success":
@@ -36,11 +36,16 @@ def process_text_service(parsed_text):
 
 
 if __name__ == "__main__":
+    audio_file = open(os.path.join("audio_text_parser", "test_files", "tyrone_enroll.wav"), "rb")
+    audio_file_identify = open(os.path.join("audio_text_parser", "test_files", "tyrone_enroll.wav"), "rb")
 
-    w = wave.open(os.path.join(os.path.relpath(__file__), "..", "audio_text_parser", "test_files", "recording.wav"),
-                  "rb")
-    binary_data = w.readframes(w.getnframes())
-    w.close()
+    enroll_user(create_profile(), audio_file)
+    if auth_user(identify_user(get_all_profiles(), audio_file_identify)).get('Confidence') == 'High':
+        w = wave.open(os.path.join("audio_text_parser", "test_files", "tyrone_enroll.wav"), "rb")
+        binary_data = w.readframes(w.getnframes())
+        w.close()
 
-    output = AudioTextParser(binary_data).parse_audio()
-    process_text_service(output)
+        output = AudioTextParser(binary_data).parse_audio()
+        process_text_service(output)
+    else:
+        print "Please Try Again! User not found with High Confidence!"
